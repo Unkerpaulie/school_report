81a3ccff44c1e7609bca083e14d00afd5e7cdcc0
Paul Pedro <unkerpaulie@gmail.com>
2025-05-03 10:21:30 -0400

    School year setup refactored for multipl year addistion

--- school_report/academics/urls.py
+++ school_report/academics/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'academics'
 
 urlpatterns = [
-    path('school-year/', views.SchoolYearSetupView.as_view(), name='school_year_setup'),
+    # Year management
+    path('years/', views.YearListView.as_view(), name='year_list'),
+    path('years/add/', views.SchoolYearSetupView.as_view(), name='school_year_setup'),
+    path('years/<int:pk>/edit/', views.YearUpdateView.as_view(), name='year_update'),
+    path('years/<int:pk>/delete/', views.YearDeleteView.as_view(), name='year_delete'),
 ]
--- school_report/academics/views.py
+++ school_report/academics/views.py
@@ -1,13 +1,91 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.utils import timezone
 from django.urls import reverse_lazy
+from django.db.models import Q
 from .models import Year
 from schools.models import School
+from core.mixins import SchoolAdminRequiredMixin, SchoolAccessRequiredMixin
 
-class SchoolYearSetupView(LoginRequiredMixin, CreateView):
+class YearListView(LoginRequiredMixin, SchoolAdminRequiredMixin, ListView):
+    """
+    View for listing academic years
+    """
+    model = Year
+    template_name = 'academics/year_list.html'
+    context_object_name = 'years'
+
+    def get_queryset(self):
+        # Get all years, ordered by start_year (descending)
+        return Year.objects.all().order_by('-start_year')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get the current year and term
+        current_year_data = get_current_school_year_and_term(self.request)
+        context['current_year'] = current_year_data.get('current_year')
+
+        return context
+
+
+class YearUpdateView(LoginRequiredMixin, SchoolAdminRequiredMixin, UpdateView):
+    """
+    View for updating an existing academic year
+    """
+    model = Year
+    template_name = 'academics/year_form.html'
+    fields = ['start_year', 'term1_start_date', 'term1_end_date', 'term1_school_days',
+              'term2_start_date', 'term2_end_date', 'term2_school_days',
+              'term3_start_date', 'term3_end_date', 'term3_school_days']
+
+    def get_success_url(self):
+        return reverse_lazy('academics:year_list', kwargs={'school_slug': self.school_slug})
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Clear any existing session data to force recalculation
+        if 'current_year_id' in self.request.session:
+            del self.request.session['current_year_id']
+        if 'current_term' in self.request.session:
+            del self.request.session['current_term']
+        if 'is_on_vacation' in self.request.session:
+            del self.request.session['is_on_vacation']
+
+        messages.success(self.request, "School year has been updated successfully!")
+        return response
+
+
+class YearDeleteView(LoginRequiredMixin, SchoolAdminRequiredMixin, DeleteView):
+    """
+    View for deleting an academic year
+    """
+    model = Year
+    template_name = 'academics/year_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('academics:year_list', kwargs={'school_slug': self.school_slug})
+
+    def delete(self, request, *args, **kwargs):
+        year = self.get_object()
+
+        # Check if this year has any related records
+        if (year.standard_teachers.exists() or
+            year.enrollments.exists() or
+            year.standard_subjects.exists() or
+            year.term_tests.exists()):
+            messages.error(request, "Cannot delete this academic year because it has related records.")
+            return redirect(self.get_success_url())
+
+        # If no related records, proceed with deletion
+        messages.success(request, "Academic year has been deleted successfully!")
+        return super().delete(request, *args, **kwargs)
+
+
+class SchoolYearSetupView(LoginRequiredMixin, SchoolAdminRequiredMixin, CreateView):
     """
     View for setting up the school year
     """
@@ -69,9 +147,9 @@ class SchoolYearSetupView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         """
-        Redirect to the dashboard after successful setup
+        Redirect to the year list after successful setup
         """
-        return reverse_lazy('schools:dashboard')
+        return reverse_lazy('academics:year_list', kwargs={'school_slug': self.school_slug})
 
 def get_current_school_year_and_term(request):
     """
